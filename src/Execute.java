d4c8aad7f5667efe575606b94f288c9294e82adf
Amrin <amrin.sandhar@gmail.com>
2022-03-04 03:08:04 -0800

    added Scenario field to execution

--- src/Execute.java
+++ src/Execute.java
@@ -1,27 +1,40 @@
 public class Execute {
 
     public static void main(String[] args) {
-        GoalGenerator gg = new GoalGenerator();
+        Scenario scenario = Scenario.RANDOM;
 
+        GoalGenerator gg = new GoalGenerator();
         double goalAngle = gg.getGoalAngle();
         double turretAngle = gg.generateRandomAngle();
-
-        //Make sure goal agle and turret angle are not the same
-        while(goalAngle == turretAngle)
-            turretAngle = gg.generateRandomAngle();
 
         System.out.println("Generated goal angle : " + goalAngle);
         System.out.println("Generated turret angle : " + turretAngle);
         System.out.println();
 
-        Turret t = new Turret(goalAngle, turretAngle);
-        PrimitiveTurret p = new PrimitiveTurret(goalAngle, turretAngle);
+        Turret t;
+        PrimitiveTurret p;
 
-        //Turret t = new Turret(Turret.MIN_POSITION, Turret.MAX_POSITION);
-        //wPrimitiveTurret p = new PrimitiveTurret(Turret.MIN_POSITION, Turret.MAX_POSITION);
+        switch(scenario) {
+            case RANDOM:
+                //Make sure goal angle and turret angle are not the same
+                while(goalAngle == turretAngle)
+                    turretAngle = gg.generateRandomAngle();
 
-        //Turret t = new Turret(30.0, 200.0);
-        //PrimitiveTurret p = new PrimitiveTurret(30.0, 200.0);
+                t = new Turret(goalAngle, turretAngle);
+                p = new PrimitiveTurret(goalAngle, turretAngle);
+                break;
+            case MAX:
+                t = new Turret(Turret.MIN_POSITION, Turret.MAX_POSITION);
+                p = new PrimitiveTurret(Turret.MIN_POSITION, Turret.MAX_POSITION);
+                break;
+            case CUSTOM:
+                t = new Turret(30.0, 200.0);
+                p = new PrimitiveTurret(30.0, 200.0);
+                break;
+            default:
+                t = new Turret(0,0);
+                p = new PrimitiveTurret(0,0);
+        }
 
         while(!t.locked){
             t.operate();
@@ -30,5 +43,11 @@ public class Execute {
 
         System.out.println("Target locked in [" + t.moves + "] moves.");
         //System.out.println("Primitive locked in [" + p.getMovesToLock() + "] moves.");
+    }
+
+    public enum Scenario {
+        RANDOM,
+        MAX,
+        CUSTOM;
     }
 }
